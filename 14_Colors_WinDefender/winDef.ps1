21722c59d0cf43bdd64b33b4a53e3aab1212f29e
r0zehnal0vak <k.rozehnalka@seznam.cz>
2024-01-10 19:17:44 +0100

    uloha 14 commit 3

--- 14_Colors_WinDefender/winDef.ps1
+++ 14_Colors_WinDefender/winDef.ps1
@@ -6,4 +6,4 @@ $threatCount = $threats.Count
 Write-Host "Počet detekcí hrozeb: $threatCount" # počet různých hrozeb, které dokáže detekovat
 
 $mostDetectedThreats = $threats| Sort-Object -Property DetectionCount -Descending | Select-Object -First 1 # Typ nejvíce detekované hrozby
-Write-Host "Nejvíce detekovaná hrozba: $($mostDetectedThreats.ThreatName)"
+Write-Host "Nejvíce detekovaná hrozba: $mostDetectedThreats"
