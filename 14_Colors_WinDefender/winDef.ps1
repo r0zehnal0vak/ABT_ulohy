f719bf031d800fe88d841f07558baeaf5b92c7ff
r0zehnal0vak <k.rozehnalka@seznam.cz>
2024-01-13 15:54:37 +0100

    uloha 14 - opravy

--- 14_Colors_WinDefender/winDef.ps1
+++ 14_Colors_WinDefender/winDef.ps1
@@ -1,9 +1,14 @@
-Update-MpSignature # aktualizace
+# Set-MpPreference -DisableRealtimeMonitoring $true
+Update-MpSignature # aktualizace DB
 
 $threats = Get-MpThreatCatalog
 $threatCount = $threats.Count
 
 Write-Host "Počet detekcí hrozeb: $threatCount" # počet různých hrozeb, které dokáže detekovat
 
-$mostDetectedThreats = $threats| Sort-Object -Property DetectionCount -Descending | Select-Object -First 1 # Typ nejvíce detekované hrozby
-Write-Host "Nejvíce detekovaná hrozba: $mostDetectedThreats"
+$rozdelovac = $threats | ForEach-Object { $_.ThreatName.Split(':')[0] }
+$hrozby = $rozdelovac | Group-Object | Sort-Object -Property Count -Descending
+$typHrozby = $hrozby | Select-Object -First 5 # prvních 5 hrozeb
+
+$typHrozbyNazvy = $typHrozby.Name
+Write-Host "Nejčastější typy hrozeb: $typHrozbyNazvy"
